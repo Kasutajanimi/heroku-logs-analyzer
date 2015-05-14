90fee95e9223763b3b0d548505d717ac28a6dbe4
kristionas <kristionas@yandex.ru>
2015-05-14 08:25:58 +0700

    enhance output

--- heroku_logs_analyzer.rb
+++ heroku_logs_analyzer.rb
@@ -66,14 +66,22 @@ class HerokuLogsAnalyzer
     moda = x0 + h*(f - f_prev)/((f - f_prev) + (f - f_next))
   end
 
-  def find_mean_time request_times, cnt_lines
-    mean_time = request_times.inject{|sum, x| sum + x } / cnt_lines
+  def find_mean_time request_times
+    mean_time = request_times.inject{|sum, x| sum + x } / @cnt_lines
   end
 
   def find_median mean_time, mode
     # median formula
     # me = 0.5*mode + 2*mean_time/3
     median = 0.5*mode + 2*mean_time/3
+  end
+
+  def analyse_timings request_times, times
+    res = Hash.new
+    res[:mean_time] = find_mean_time request_times
+    res[:mode] = find_mode times
+    res[:median] = find_median res[:mean_time], res[:mode]
+    res
   end
 
   def count_dynos dynos, dyno
@@ -102,16 +110,15 @@ class HerokuLogsAnalyzer
     cnt_get_friends_score = 0
     cnt_post_user = 0
     cnt_get_user = 0
-    cnt_lines = 0
+    @cnt_lines = 0
 
     request_times = Array.new
 
-    dynos = Array.new #Hash.new
+    dynos = Array.new # array of hashes
     times = Array.new
 
     @log_file.each_line do |line|
-      cnt_lines += 1
-      #yield line
+      @cnt_lines += 1
 
       connect_time = line.match(/connect=[0-9]*ms/).to_s.split("=").last.split("ms").first
       service_time = line.match(/service=[0-9]*ms/).to_s.split("=").last.split("ms").first
@@ -191,84 +198,62 @@ class HerokuLogsAnalyzer
         times[5] ||= Array.new
         times[5] = count_time times[5], total_time
       end
-
-      #dyno=web.8
-      #unless dynos[dyno.to_sym].nil?
-      #  dynos[dyno.to_sym] += 1
-      #else
-      #  dynos[dyno.to_sym] = 1
-      #end
-
-      #request_times << total_time
-
-      #unless times[total_time].nil?
-      #  times[total_time] += 1
-      #else
-      #  times[total_time] = 1
-      #end
     end
 
-    puts "Total entries: " + cnt_lines.to_s + ".\n\n"
+    timings_data = Array.new
 
-    puts "GET /api/users/{user_id}/count_pending_messages called " + cnt_get_count_pending_messages.to_s + " times."
-    puts "Busiest dyno: " + show_busiest_dynos(dynos[0]).to_s
-    mean_time = find_mean_time request_times[0], cnt_lines
-    mode = find_mode times[0]
-    median = find_median mean_time, mode
-    puts "Mean time: " + mean_time.to_s
-    puts "Mode: " + mode.to_s
-    puts "Median: " + median.to_s
-    puts "\n\n"
+    (0..5).each do |i|
+      timings_data[i] = analyse_timings request_times[i], times[i]
+    end
 
-    puts "GET /api/users/{user_id}/get_messages called " + cnt_get_messages.to_s + " times."
-    puts "Busiest dyno: " + show_busiest_dynos(dynos[1]).to_s
-    mean_time = find_mean_time request_times[1], cnt_lines
-    mode = find_mode times[1]
-    median = find_median mean_time, mode
-    puts "Mean time: " + mean_time.to_s
-    puts "Mode: " + mode.to_s
-    puts "Median: " + median.to_s
-    puts "\n\n"
+    output = <<-EOT
 
-    puts "GET /api/users/{user_id}/get_friends_progress called " + cnt_get_friends_progress.to_s + " times."
-    puts "Busiest dyno: " + show_busiest_dynos(dynos[2]).to_s
-    mean_time = find_mean_time request_times[2], cnt_lines
-    mode = find_mode times[2]
-    median = find_median mean_time, mode
-    puts "Mean time: " + mean_time.to_s
-    puts "Mode: " + mode.to_s
-    puts "Median: " + median.to_s
-    puts "\n\n"
+      Heroku Logs Analyzer by Kasutajanimi, (c) 2015
+      GitHub: https://github.com/Kasutajanimi/heroku-logs-analyzer
 
-    puts "GET /api/users/{user_id}/get_friends_score called " + cnt_get_friends_score.to_s + " times."
-    puts "Busiest dyno: " + show_busiest_dynos(dynos[3]).to_s
-    mean_time = find_mean_time request_times[3], cnt_lines
-    mode = find_mode times[3]
-    median = find_median mean_time, mode
-    puts "Mean time: " + mean_time.to_s
-    puts "Mode: " + mode.to_s
-    puts "Median: " + median.to_s
-    puts "\n\n"
+      Analysing #{@log_file.path}...
 
-    puts "POST /api/users/{user_id} called " + cnt_post_user.to_s + " times."
-    puts "Busiest dyno: " + show_busiest_dynos(dynos[4]).to_s
-    mean_time = find_mean_time request_times[4], cnt_lines
-    mode = find_mode times[4]
-    median = find_median mean_time, mode
-    puts "Mean time: " + mean_time.to_s
-    puts "Mode: " + mode.to_s
-    puts "Median: " + median.to_s
-    puts "\n\n"
+      Total queries: #{@cnt_lines.to_s}
 
-    puts "GET /api/users/{user_id} called " + cnt_get_user.to_s + " times."
-    puts "Busiest dyno: " + show_busiest_dynos(dynos[5]).to_s
-    mean_time = find_mean_time request_times[5], cnt_lines
-    mode = find_mode times[5]
-    median = find_median mean_time, mode
-    puts "Mean time: " + mean_time.to_s
-    puts "Mode: " + mode.to_s
-    puts "Median: " + median.to_s
-    puts "\n\n"
+      1) GET "/api/users/{user_id}/count_pending_messages" called #{cnt_get_count_pending_messages.to_s} times.
+      Busiest dynos: #{show_busiest_dynos(dynos[0]).to_s}
+      Mean time: #{timings_data[0][:mean_time].to_s}
+      Mode: #{timings_data[0][:mode].to_s}
+      Median: #{timings_data[0][:median].to_s}
+
+      2) GET "/api/users/{user_id}/get_messages" called #{cnt_get_messages.to_s} times.
+      Busiest dynos: #{show_busiest_dynos(dynos[1]).to_s}
+      Mean time: #{timings_data[1][:mean_time].to_s}
+      Mode: #{timings_data[1][:mode].to_s}
+      Median: #{timings_data[1][:median].to_s}
+
+      3) GET "/api/users/{user_id}/get_friends_progress" called #{cnt_get_friends_progress.to_s} times.
+      Busiest dynos: #{show_busiest_dynos(dynos[2]).to_s}
+      Mean time: #{timings_data[2][:mean_time].to_s}
+      Mode: #{timings_data[2][:mode].to_s}
+      Median: #{timings_data[2][:median].to_s}
+
+      4) GET "/api/users/{user_id}/get_friends_score" called #{cnt_get_friends_score.to_s} times.
+      Busiest dynos: #{show_busiest_dynos(dynos[3]).to_s}
+      Mean time: #{timings_data[3][:mean_time].to_s}
+      Mode: #{timings_data[3][:mode].to_s}
+      Median: #{timings_data[3][:median].to_s}
+
+      5) POST "/api/users/{user_id}" called #{cnt_post_user.to_s} times.
+      Busiest dynos: #{show_busiest_dynos(dynos[4]).to_s}
+      Mean time: #{timings_data[4][:mean_time].to_s}
+      Mode: #{timings_data[4][:mode].to_s}
+      Median: #{timings_data[4][:median].to_s}
+
+      6) GET "/api/users/{user_id}" called #{cnt_get_user.to_s} times.
+      Busiest dynos: #{show_busiest_dynos(dynos[5]).to_s}
+      Mean time: #{timings_data[5][:mean_time].to_s}
+      Mode: #{timings_data[5][:mode].to_s}
+      Median: #{timings_data[5][:median].to_s}
+
+    EOT
+
+    puts output
   end
 end
 
